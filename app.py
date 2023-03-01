38321378cd67ff3c2387ccae5cc0fa595d61cba9
Yonghye Kwon <developer.0hye@gmail.com>
2023-03-01 23:15:04 +0900

    pre-commit test

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+
+def func(
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+):
+    pass
+
+
+a = 1 + 2
+b = 1 + 2
+c = 1 + 2
+d = 1 + 2
+e = 1 + 2
+f = 1 + 2
+
+l = [i for i in range(10)]
+
+print(l)
+print(a, b, c, d, e, f)
